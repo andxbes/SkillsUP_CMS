e9dbaf2a66eeb7a5cf6f6a3583d75f1daedc00b4
Andr <Andr@AndreyPC>
2014-09-23 20:28:26 +0400

    only arhitype

---
+++ site/src/main/java/ua/skillsup/skillsup_cms/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package ua.skillsup.skillsup_cms.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="SkillsUP_CMS:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
